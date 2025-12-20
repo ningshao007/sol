9bd7837ba9d477b75b9a2600caba867f5b4d44cc
nathan <ningshao01@outlook.com>
2025-12-20 12:23:26 +0800

    chore: update code

--- 27.sol
+++ 27.sol
@@ -2,23 +2,25 @@
 pragma solidity ^0.8.21;
 
 contract OnlyEven {
+    error InvalidNumber();
+    error NotEven();
+
     constructor(uint a) {
-        require(a != 0, "invalid number");
-        assert(a != 1);
+        if (a == 0 || a == 1) revert InvalidNumber();
     }
 
-    function onlyEven(uint256 b) external pure returns (bool success) {
-        require(b % 2 == 0, "ups!Reverting");
-        success = true;
+    function onlyEven(uint256 b) external pure returns (bool) {
+        if (b % 2 != 0) revert NotEven();
+        return true;
     }
 }
 
 contract TryCatch {
-    event SuccessEvent();
-    event CatchEvent(string message);
-    event CatchByte(bytes data);
+    event Success();
+    event ErrorReason(string message);
+    event PanicData(bytes data);
 
-    OnlyEven even;
+    OnlyEven immutable even;
 
     constructor() {
         even = new OnlyEven(2);
@@ -26,21 +28,23 @@ contract TryCatch {
 
     function execute(uint amount) external returns (bool success) {
         try even.onlyEven(amount) returns (bool _success) {
-            emit SuccessEvent();
+            emit Success();
             return _success;
         } catch Error(string memory reason) {
-            emit CatchEvent(reason);
+            emit ErrorReason(reason);
+        } catch (bytes memory reason) {
+            emit PanicData(reason);
         }
     }
 
     function executeNew(uint a) external returns (bool success) {
         try new OnlyEven(a) returns (OnlyEven _even) {
-            emit SuccessEvent();
+            emit Success();
             success = _even.onlyEven(a);
         } catch Error(string memory reason) {
-            emit CatchEvent(reason);
+            emit ErrorReason(reason);
         } catch (bytes memory reason) {
-            emit CatchByte(reason);
+            emit PanicData(reason);
         }
     }
 }
