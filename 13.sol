7a773616fb45dcc58a50b867145f5233aba30c9b
nathan <ningshao01@outlook.com>
2025-12-21 18:54:29 +0800

    chore: update code

--- 13.sol
+++ 13.sol
@@ -3,11 +3,25 @@ pragma solidity ^0.8.21;
 
 abstract contract Base {
     string public name = "base";
-    function getAlias() public pure virtual returns (string memory);
+    function getAlias() external pure virtual returns (string memory);
+
+    function setName(string memory newName) public {
+        name = newName;
+    }
 }
 
 contract BaseImpl is Base {
-    function getAlias() public pure override returns (string memory) {
+    function getAlias() external pure override returns (string memory) {
         return "BaseImpl";
+    }
+}
+
+contract Child is Base {
+    constructor() {
+        setName("child");
+    }
+
+    function updateName(string memory newName) external {
+        setName(newName);
     }
 }
