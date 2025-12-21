efcc61f05ecf2c9aebc38f80a08092415ba0ba2e
nathan <ningshao01@outlook.com>
2025-12-21 11:35:29 +0800

    chore: update code

--- 24.sol
+++ 24.sol
@@ -24,4 +24,19 @@ contract DeployContract {
         // 部署者(外部调用者)需在返回地址上自行调用 deleteContract()
         return res;
     }
+
+    function demoAndDestroyAsOwner() public payable returns (DemoResult memory) {
+        // 让本合约成为 owner, 以便在同一交易内自毁
+        DeleteContract del = new DeleteContract{value: msg.value}(address(this));
+
+        DemoResult memory res = DemoResult({
+            addr: address(del),
+            balance: del.getBalance(),
+            value: del.value()
+        });
+
+        del.deleteContract();
+
+        return res;
+    }
 }
