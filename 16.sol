5619205c744db62cb5d4f3244e360fcbf1270b8a
nathan <ningshao01@outlook.com>
2025-12-21 12:44:21 +0800

    chore: update code

--- 16.sol
+++ 16.sol
@@ -1,17 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "./12.sol";
 import {Yeye} from "./12.sol";
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Import {
     using Address for address;
-    Yeye yeye = new Yeye();
+    Yeye public immutable yeye;
+
+    constructor() {
+        yeye = new Yeye();
+    }
 
     function test() external {
         yeye.hip();
+    }
+
+    function testWithAddress() external {
+        address(yeye).functionCall(abi.encodeWithSignature("hip()"));
     }
 }
